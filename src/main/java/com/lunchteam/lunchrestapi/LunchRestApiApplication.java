674509cac76ea6e85c7d9bb70f8e44df303aa01f
Rap0d <40348849+Rap0d@users.noreply.github.com>
2021-07-20 13:18:58 +0900

    first commit

---
+++ src/main/java/com/lunchteam/lunchrestapi/LunchRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.lunchteam.lunchrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchRestApiApplication.class, args);
+	}
+
+}
