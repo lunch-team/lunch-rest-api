053c63fe8ed75a6bbded2dc540a790d93d3ac4d0
rapod <changsh9155@gmail.com>
2021-12-17 15:07:28 +0900

    로그인 상태 체크 시 에러발생했을 때 401 내려주도록 수정

--- src/main/java/com/lunchteam/lunchrestapi/api/controller/MemberController.java
+++ src/main/java/com/lunchteam/lunchrestapi/api/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.lunchteam.lunchrestapi.api.controller;
 
 import com.lunchteam.lunchrestapi.api.dto.member.MemberResponseDto;
+import com.lunchteam.lunchrestapi.api.exception.AuthenticationException;
+import com.lunchteam.lunchrestapi.api.response.BasicResponse;
+import com.lunchteam.lunchrestapi.api.response.ErrorResponse;
+import com.lunchteam.lunchrestapi.api.response.StatusEnum;
 import com.lunchteam.lunchrestapi.api.service.MemberService;
+import com.lunchteam.lunchrestapi.handler.ResultHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +23,20 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/me")
-    public ResponseEntity<MemberResponseDto> getMyMemberInfo() {
-        return ResponseEntity.ok(memberService.getMyInfo());
+    public ResponseEntity<? extends BasicResponse> getMyMemberInfo() {
+
+        try {
+            MemberResponseDto result = memberService.getMyInfo();
+            if (result != null) {
+                return ResultHandler.setResult(result, HttpStatus.UNAUTHORIZED);
+            } else {
+                throw new AuthenticationException();
+            }
+        } catch (Exception e) {
+            return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse(StatusEnum.INTERNAL_SERVER_ERROR));
+        }
     }
 
     @GetMapping("/{email}")
